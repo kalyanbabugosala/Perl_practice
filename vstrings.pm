fd98478a8883b841501c2494dd95b51eff95cb46
Kalyan <kalyanbabu.gosala@thundersoft.com>
2021-11-05 19:41:47 +0530

    Created vstrings

---
+++ vstrings.pm
@@ -0,0 +1,16 @@
+=head
+#V strings
+
+$smile  = v9786;
+$foo    = v102.111.111;
+$martin = v77.97.114.116.105.110;
+print "smile = $smile\n";
+print "foo = $foo\n";
+print "martin = $martin\n";
+=cut
+
+#special literals
+
+print "File name ". __FILE__ . "\n"; 
+print "Line Number " . __LINE__ ."\n";
+print "Package " . __PACKAGE__ ."\n";

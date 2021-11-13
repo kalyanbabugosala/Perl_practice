5b5128299765c510e6f2ce02ad9978a9feaea887
Kalyan <kalyanbabu.gosala@thundersoft.com>
2021-11-13 12:17:39 +0530

    previous modifications

--- File_IO.pm
+++ File_IO.pm
@@ -1,16 +1,33 @@
 =head
-open(DATA, ">File1.txt") or die "Couldn't open file File1.txt, $!";
+# <-read,  >-write,   >>-append.
+open(w, ">>","File1.txt") or die "Couldn't open file File1.txt, $!";
 
-while(<DATA>) {
+#=head
+if(<DATA>)
+{
+	print "Data is there";
+}
+else
+{
+	print "NO data found";
+}
+#=cut
+#print <DATA>; 
+
+print w "add to previous content";
+
+seek w,0,2;
+
+while(<w>) {
    print "$_";
 }
 =cut
 
-#=head
+=head
 open(R,">Hello.txt") or die "Could not open the File1.txt, $!";
 
 print ("Data in file is:".<R>);
-#=cut
+=cut
 
 =head
 if(R != EOF)
@@ -27,7 +44,7 @@ else
 
 
 =head
-#!/usr/bin/perl
+
 
 # Opening File Hello.txt in Read mode
 open(r, "<", "Hello.txt");
@@ -36,10 +53,10 @@ open(r, "<", "Hello.txt");
 print("Existing Content of Hello.txt: " . <r>);
 
 # Opening File in Write mode
-open(w, ">", "Hello.txt");
+open(w, ">>", "Hello.txt");
 
-# Set r to the beginning of Hello.txt
-seek r, 0, 0;
+# Set r to the End of Hello.txt
+#seek r, 0,2;#SEEK_SET,SEEK_CUR,SEEK_END => 0,1,2.
 
 print "\nWriting to File...";
 
@@ -53,19 +70,69 @@ close(w);
 seek r, 0, 0;
 
 # Print the current contents of Hello.txt
-print("\nUpdated Content of Hello.txt: ".<r>);
+@lines = <r>;
+print @lines;
+#print("\nUpdated Content of Hello.txt: ".<r>);
 
 # Close the FileHandle
 close(r);
 =cut
 
 
+#Get the file description or file information.
+=head
+my $file = "1.exe";
+my (@description, $size);
+if (-e $file) {
+   push @description, 'binary' if (-B _);
+   push @description, 'a socket' if (-S _);
+   push @description, 'a text file' if (-T _);
+   push @description, 'a block special file' if (-b _);
+   push @description, 'a character special file' if (-c _);
+   push @description, 'a directory' if (-d _);
+   push @description, 'executable' if (-x _);
+   push @description, (($size = -s _)) ? "$size bytes" : 'empty';
+   print "$file is ", join(', ',@description),"\n";
+}
+=cut
 
 
 
+=head
+#Copy from one file to another.
 
+# Open file to read
+open(DATA1, "<Hello.txt");
 
+# Open new file to write
+open(DATA2, ">Destination.txt");
 
+# Copy data from one file to another.
+while(<DATA1>) {
+	#print $_;
+   print DATA2 $_;
+}
+close( DATA1 );
+close( DATA2 );
+=cut
+
+=head
+#Reading data from File
+open(DATA,"<Destination.txt") or die "Can't open data";
+@lines = <DATA>;
+print @lines;
+close(DATA);
+=cut
+
+#Renaming a file name
+=head
+rename ("\Destination.txt", "\Dest.txt" );
+=cut
+
+#Deleting existing file
+=head
+unlink ("File1.txt");
+=cut
 
 
 
